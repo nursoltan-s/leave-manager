a17765a9e5784e2363732904b01fae3603bdf55a
Nursoltan Saipolda <nursoltan.s@gmail.com>
2023-10-14 13:13:02 +0800

    init

---
+++ HRLeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,14 @@
+﻿using HRLeaveManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLeaveManagement.Domain;
+
+public class LeaveType : BaseEntity
+{ 
+    public string Name { get; set; } = string.Empty;
+    public string DefaultDays { get; set; } = string.Empty;
+}
